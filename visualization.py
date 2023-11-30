a29b2af863b459da9cb51f42b6c9e4a865c745f0
suha <75898031+hp0724@users.noreply.github.com>
2023-11-30 16:18:29 +0900

    Add files via upload

---
+++ visualization.py
@@ -0,0 +1,31 @@
+from visual_utils import visualize_utils as V
+import pickle
+import os
+from mayavi import mlab
+import torch
+
+mlab.init_notebook()
+
+for i in range(10):
+    data_dir = 'data_dict/data_dict%06d.pkl' % i
+    pred_dir = 'pred_dict/pred_dict%06d.pkl' % i
+    print('{0:06d} Open Start'.format(i))
+    # 피클 파일 로드
+    with open(data_dir, 'rb') as f:
+        data_dict = pickle.load(f)
+
+    with open(pred_dir, 'rb') as f:
+        pred_dicts = pickle.load(f)
+
+    print('{0:06d} Open Success'.format(i))
+    # 예시: Open3D를 사용한 시각화
+    if len(data_dict) > 0:
+        mlab.options.offscreen = True
+        # Open3D를 사용한 시각화
+        V.draw_scenes(
+            points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'],
+            ref_scores=pred_dicts[0]['pred_scores'], ref_labels=pred_dicts[0]['pred_labels']
+        )
+        result_dir = 'image/result%06d.png' % i
+        mlab.savefig(result_dir)
+        print('{0:06d} Finish'.format(i))
